8255ddefadaef6d6d1217ae440bbf48249ab31f7
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-02-11 18:51:33 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
 
 int main(int argc, char* argv[]) {
 	// Flush after every std::cout / std:cerr
@@ -14,10 +16,39 @@ int main(int argc, char* argv[]) {
 		std::string input;
 		std::getline(std::cin, input);
 
-		if (input == "exit")
+		std::stringstream ss(input);
+		std::string word;
+		std::vector<std::string> words;
+
+		while (ss >> word) {   // trennt automatisch nach whitespace
+			words.push_back(word);
+		}
+
+		/*for (const auto& w : words) {
+			std::cout << w << std::endl;
+		}*/
+
+		// Leave the Shell
+		if (words[0] == "exit")
 		{
 			break;
 		}
+
+		// Echo Command
+		else if (words[0] == "echo")
+		{
+			for (size_t i = 1; i < words.size(); ++i)
+			{
+				std::cout << words[i];
+				if (i < words.size() - 1)
+				{
+					std::cout << " ";
+				}
+			}
+			std::cout << "\n";
+		}
+
+		// Default
 		else
 		{
 			std::cout << input << ": command not found\n";
