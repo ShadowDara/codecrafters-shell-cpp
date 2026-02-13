dc83152cab03badd771f76c76721139d069d3361
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-02-13 11:23:48 +0100

    codecrafters submit [skip ci]

--- src/shell.cpp
+++ src/shell.cpp
@@ -23,19 +23,7 @@ std::vector<std::string> Shell::parseLine(std::string line)
 	for (size_t i = 0; i < line.size(); i++)
 	{
 		// ESCAPE
-		if (!doubleQuote && !singleQuote && line[i] == '\\')
-		{
-			// Skip the next Character
-			if (i + 1 < line.size())
-			{
-				word += line[i + 1];
-				i++;
-			}
-			continue;
-		}
-
-		// ESCAPE 2
-		if (line[i] == '\\')
+		if (!doubleQuote && line[i] == '\\')
 		{
 			// Skip the next Character
 			if (i + 1 < line.size())
